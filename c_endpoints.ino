558cc4d44cdd0f22b4e9547100aa84c7e4deb1df
Vanderlei <hdelei@gmail.com>
2021-09-12 23:18:07 -0300

    Endpoints initial stage

--- c_endpoints.ino
+++ c_endpoints.ino
@@ -1,15 +1,62 @@
-void LoadEndpoints() {
-  server.on("/", []() {
-    if (!server.authenticate(www_username, www_password)) {
-      return server.requestAuthentication();
-    }
-    server.send(200, "application/json", "{\"response\":\"ok\"}");
-  });
+byte noAuthCount = 0;
 
-  server.begin();
-  
+void getAuthorization()
+{
+  if (noAuthCount > 4)
+  {
+    Serial.println();
+    Serial.print("Locked");
+
+    server.send(423, "application/json", "{\"response\":\"Locked\"}");
+    delay(30000);
+    //return;
+  }
+
+  if (!server.authenticate(www_username, www_password))
+  {
+    noAuthCount++;
+    if (noAuthCount == 254)
+      noAuthCount = 5;
+
+    Serial.println();
+    Serial.print("NoAuthCount: " + String(noAuthCount));
+
+    return server.requestAuthentication();
+    //server.send(401, "application/json", "{\"response\":\"Unauthorized\"}");
+    //return;
+  }
+  noAuthCount = 0;
   Serial.println();
-  Serial.print("Open http://");
-  Serial.print(WiFi.localIP());
-  Serial.println("/ in your browser to see it working");
+  Serial.print("NoAuthCount: " + String(noAuthCount));
 }
+
+void handleStatus()
+{
+  if (server.method() != HTTP_POST)
+    server.send(405, "application/json", "{\"status\":\"method not allowed\"}");
+
+  getAuthorization();
+  // if (!server.authenticate(www_username, www_password))
+  //   server.send(401, "application/json", "{\"response\":\"Unauthorized\"}");
+
+  server.send(200, "application/json", "{\"status\":\"armed\"}");
+}
+
+// void LoadEndpoints()
+// {
+//   server.on("/", []()
+//             {
+//               if (!server.authenticate(www_username, www_password))
+//               {
+//                 return server.requestAuthentication();
+//               }
+//               server.send(200, "application/json", "{\"response\":\"ok\"}");
+//             });
+
+//   server.begin();
+
+//   Serial.println();
+//   Serial.print("Open http://");
+//   Serial.print(WiFi.localIP());
+//   Serial.println("/ in your browser to see it working");
+// }
--- esp_vanguard.ino
+++ esp_vanguard.ino
@@ -3,20 +3,26 @@
 #include <ArduinoOTA.h>
 #include <ESP8266WebServer.h>
 
-
 ESP8266WebServer server(80);
 
-const char* www_username = "admin";
-const char* www_password = "esp8266";
+const char *www_username = "admin";
+const char *www_password = "esp8266";
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   ConnectWiFi();
   ArduinoOTA.begin();
-  LoadEndpoints();  
+  server.on("/status", handleStatus);
+
+  server.begin();
+
+  Serial.println();
+  Serial.print(WiFi.localIP());
 }
 
-void loop() {
+void loop()
+{
   ArduinoOTA.handle();
   server.handleClient();
 }
